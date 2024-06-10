2dfba9f4c081778e324ce4c009fae5db6b7e60f3
Tomas Roa <tomas.roa@gmail.com>
2024-06-10 11:49:31 -0400

    Se corrigen rutas, se agregan funciones solicitadas en el desafio

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,29 @@
-import { addUserQueries } from "../models/userModel.js";
+import { addUserQueries, getUserQueries } from "../models/userModel.js";
+import path from "path";
+const __dirname = path.resolve();
 
 export const home = (req, res) => {
-    res.send("Hola desde el controlador de usuarios");
+    res.sendFile(__dirname + "/views/index.html");
 }
 
 export const addUser = async (req, res) => {
     const {nombre, balance} = req.body;
+    console.log(req.body)
     try {
         await addUserQueries(nombre, balance);
-        res.send("Usuario insertado")
+        res.status(201).send("Usuario creado con exito");
     } catch (error) {
-        console.log("ERROR CODE: ", error.code, "MESSAGE: ", error.message);
+        console.log(error);
+        res.status(500).send("Algo salio mal al insertar el usuario");
     }
 }
 
+export const getUsers = async (req, res) => {
+    try {
+        const users = await getUserQueries();
+        res.status(201).send(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Algo salio mal al obtener los usuarios");
+    }
+}
--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,33 @@
-import { pool } from "../config/db";
+import { pool } from "../config/db.js";
 
-export const getUserQueries = async (nombre, balance) => {
+export const addUserQueries = async (nombre, balance) => {
     try {
         const sql = {
-            text: "INSERT INTO usuarios (nombre, balance) VALUES ($1, $2) *",
+            text: "INSERT INTO usuarios (nombre, balance) VALUES ($1, $2) returning *",
             values: [nombre, balance]
         }
 
         const response = await pool.query(sql);
         if (response.rowCount > 0) {
-            return response.rows
+            return response.rows 
         } else {
             return new Error("No se inserto el usuario")
+        }
+    } catch (error) {
+        console.log("ERROR CODE: ", error.code, "MESSAGE: ", error.message);
+    }
+}
+
+export const getUserQueries = async() => {
+    try {
+        const sql = {
+            text: "SELECT * FROM usuarios",
+        }
+        const response = await pool.query(sql);
+        if (response.rowCount > 0) {
+            return response.rows
+        } else {
+            return new Error("No se encontraron usuarios")
         }
     } catch (error) {
         console.log("ERROR CODE: ", error.code, "MESSAGE: ", error.message);
